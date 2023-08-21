7a349b9d85175510c44ebefe4a9104d8170a854b
Alexander Fritsch <35202909+loudar@users.noreply.github.com>
2023-08-21 16:47:23 +0200

    fix break action

--- Definitions/Physical/Actions.mjs
+++ Definitions/Physical/Actions.mjs
@@ -4,7 +4,7 @@ import {Action} from "./Action.mjs";
 export class Actions {
     static BreakObject() {
         return Action.new("break")
-            .set((character, object) => {
+            .set((object) => {
                 object.setState("broken");
             })
             .addCondition(Conditions.Healthy())
